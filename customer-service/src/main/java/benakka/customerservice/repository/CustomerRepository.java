be7c50e4c0188ac5fd9d4af4d003092c70282baf
Fati89 <fatimazahraee989@gmail.com>
2026-09-23 18:05:33 +0100

    Initial commit

---
+++ customer-service/src/main/java/benakka/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package benakka.customerservice.repository;
+
+import benakka.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
